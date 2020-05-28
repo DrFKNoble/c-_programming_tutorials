15407fd35723c346b0cabec18535e1b28797ccf7
drfrazernoble <f.k.noble@massey.ac.nz>
2020-05-28 12:35:21 +1200

    Reorganised the repo's structure. Renamed listings to make CMakeLists.txt easier to reference.

---
+++ lecture_9/listing_1/listing.cpp
@@ -0,0 +1,38 @@
+#include "listing.hpp"
+
+Bike::Bike(int initialSpeed)
+{
+    setSpeed(initialSpeed);
+}
+
+Bike::~Bike()
+{
+}
+
+void Bike::pedal()
+{
+    setSpeed(speed + 1);
+    std::cout << "Pedaling. Speed: " << speed << " m/s" << std::endl;
+
+    return;
+}
+
+void Bike::brake()
+{
+    setSpeed(speed - 1);
+    std::cout << "Braking. Speed: " << speed << " m/s" << std::endl;
+
+    return;
+}
+
+int main()
+{
+
+    Bike myBike(5);
+
+    myBike.pedal();
+    myBike.pedal();
+    myBike.brake();
+
+    return 0;
+}
